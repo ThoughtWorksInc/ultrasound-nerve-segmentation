b4bb88e74101195b6f7d750351583fff1d56c67b
Tong Damon Da <tongda@outlook.com>
2016-07-13 17:23:20 +0800

    #3 [tongda] refactor training code. add visualization.

--- ultra_detection/main.py
+++ ultra_detection/main.py
@@ -1,98 +1,127 @@
+import math
+
 import tensorflow as tf
+
 from ultra_detection.input_data import read_data_sets
 
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
+
+
+def loss(y_, y_conv):
+  cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]), name='xentropy')
+  return cross_entropy
+
+
+def inference(keep_prob, x_image):
+  with tf.name_scope('conv1'):
+    # conv 1
+    weights = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=1.0 / math.sqrt(420 * 580 / 2), name='weights'))
+    biases = tf.Variable(tf.constant(0.01, shape=[32], name='biases'))
+
+    # relu + pool
+    h_conv1 = tf.nn.relu(conv2d(x_image, weights) + biases)
+    h_pool1 = max_pool_2x2(h_conv1)
+  with tf.name_scope('conv2'):
+    # conv 2
+    weights = tf.Variable(
+      tf.truncated_normal([5, 5, 32, 64], stddev=1.0 / math.sqrt(210 * 290 * 32 / 2), name='weights'))
+    biases = tf.Variable(tf.constant(0.01, shape=[64], name='biases'))
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, weights) + biases)
+    h_pool2 = max_pool_2x2(h_conv2)
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 105 * 145 * 64])
+  with tf.name_scope('fc1'):
+    # fc 1
+    weights = tf.Variable(
+      tf.truncated_normal([105 * 145 * 64, 1024], stddev=1.0 / math.sqrt(105 * 145 * 64 / 2), name='weights'))
+    biases = tf.Variable(tf.constant(0.01, shape=[1024], name='biases'))
+
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, weights) + biases)
+
+    # dropout
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+  with tf.name_scope('fc1'):
+    # fc 2
+    weights = tf.Variable(tf.truncated_normal([1024, 2], stddev=1.0 / math.sqrt(105 * 145 * 64 / 2), name='weights'))
+    biases = tf.Variable(tf.constant(0.01, shape=[2], name='biases'))
+
+    # softmax
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, weights) + biases)
+
+  return y_conv
+
+
+def training(cross_entropy):
+  tf.scalar_summary(cross_entropy.op.name, cross_entropy)
+  # solver
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+  return train_step
+
+
+def run_training(datasets):
+  with tf.Graph().as_default():
+    # input layer
+    x = tf.placeholder(tf.float32, shape=[None, 420, 580])
+    y_ = tf.placeholder(tf.float32, shape=[None, 2])
+
+    # dropout prob
+    keep_prob = tf.placeholder(tf.float32)
+
+    # reshape
+    x_image = tf.reshape(x, [-1, 420, 580, 1])
+
+    y_conv = inference(keep_prob, x_image)
+
+    # loss
+    cross_entropy = loss(y_, y_conv)
+
+    train_step = training(cross_entropy)
+
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    summary_op = tf.merge_all_summaries()
+
+    # start session
+    sess = tf.Session()
+
+    summary_writer = tf.train.SummaryWriter('train_cache', sess.graph)
+
+    init = tf.initialize_all_variables()
+
+    sess.run(init)
+
+    for i in range(20):
+      batch = datasets.train.next_batch(50)
+      if i % 5 == 0:
+        acc, loss_res = sess.run([accuracy, cross_entropy], feed_dict={
+          x: batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, loss %g, training accuracy %g" % (i, loss_res, acc))
+
+        summary_str = sess.run(summary_op, feed_dict={
+          x: batch[0], y_: batch[1], keep_prob: 1.0})
+        summary_writer.add_summary(summary_str, i)
+        summary_writer.flush()
+
+        # val_batch = datasets.validation.next_batch(10)
+        # validation_accuracy = accuracy.eval(feed_dict={
+        #     x: val_batch[0], y_: val_batch[1], keep_prob: 1.0
+        # })
+        # print("step %d, validation accuracy %g" % (i, validation_accuracy))
+      sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    print("test accuracy %g" % sess.run(accuracy, feed_dict={
+      x: datasets.test.all_images, y_: datasets.test.all_labels, keep_prob: 1.0}))
 
 
 # load data
-ultra = read_data_sets('/Users/dtong/code/data/competition/ultrasound-nerve-segmentation/train', 4000, 1000)
+ultra = read_data_sets('/Users/dtong/code/data/competition/ultrasound-nerve-segmentation/sample', 50, 10)
 
-# start session
-sess = tf.InteractiveSession()
-
-# input layer
-x = tf.placeholder(tf.float32, shape=[None, 420, 580])
-y_ = tf.placeholder(tf.float32, shape=[None, 2])
-
-# conv 1
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
-# reshape
-x_image = tf.reshape(x, [-1, 420, 580, 1])
-
-# relu + pool
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-# conv 2
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-# fc 1
-W_fc1 = weight_variable([105 * 145 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 105 * 145 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# dropout
-keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# fc 2
-W_fc2 = weight_variable([1024, 2])
-b_fc2 = bias_variable([2])
-
-# softmax
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
-# loss
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-
-# solver
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-sess.run(tf.initialize_all_variables())
-
-for i in range(400):
-    batch = ultra.train.next_batch(50)
-    if i % 20 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-
-        # TODO: seems like tensorflow will evaluate lazily, need find out.
-        print("step %d, training accuracy %g" % (i, train_accuracy))
-
-        val_batch = ultra.validation.next_batch(100)
-        validation_accuracy = accuracy.eval(feed_dict={
-            x: val_batch[0], y_: val_batch[1], keep_prob: 1.0
-        })
-        print("step %d, validation accuracy %g" % (i, validation_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-print("test accuracy %g" % accuracy.eval(feed_dict={
-    x: ultra.test.all_images, y_: ultra.test.all_labels, keep_prob: 1.0}))
+run_training(ultra)
