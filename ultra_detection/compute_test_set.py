175f814723af5bed1e690de678ed9eddad5c76cb
Tong Damon Da <tongda@outlook.com>
2016-07-24 10:33:08 +0800

    #3 [tongda] add compute submission result code.

---
+++ ultra_detection/compute_test_set.py
@@ -0,0 +1,88 @@
+import math
+import os
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from ultra_detection.model import inference
+
+
+def read_test_data(data_dir):
+  return list(map(lambda p: os.path.join(data_dir, p), filter(lambda p: p != '.DS_Store', os.listdir(data_dir))))
+
+
+def extract_img_id(path):
+  return os.path.splitext(os.path.basename(path))[0]
+
+
+def has_bp(cls):
+  return cls[0] > cls[1]
+
+
+def round_the_rect(rect, radius=10):
+  drop_pixels = [10 - int(math.sqrt(radius ** 2 - i ** 2)) for i in range(10, 0, -1)]
+
+  for i in range(radius):
+    rect[i][0] += drop_pixels[i]
+    rect[i][1] -= 2 * drop_pixels[i]
+    rect[-1 - i][0] += drop_pixels[i]
+    rect[-1 - i][1] -= 2 * drop_pixels[i]
+
+
+def generate_rle(loc):
+  points = [int(loc[0] * 420), int(loc[1] * 580), int(loc[2] * 420), int(loc[3] * 580)]
+  rect = []
+  for i in range(points[1], points[3] + 1):
+    rect.append([i * 420 + points[0], points[2] - points[0]])
+
+  round_the_rect(rect)
+
+  return ' '.join(' '.join(str(item) for item in row) for row in rect)
+
+
+def predict(data_paths):
+  with tf.Graph().as_default():
+    # input layer
+    x = tf.placeholder(tf.float32, shape=[None, 420, 580])
+
+    # dropout prob
+    keep_prob = tf.placeholder(tf.float32)
+
+    # reshape
+    x_image = tf.reshape(x, [-1, 420, 580, 1])
+
+    y_test_conv, y_test_loc = inference(keep_prob, x_image)
+
+    init = tf.initialize_all_variables()
+
+    sess = tf.Session()
+
+    sess.run(init)
+
+    saver = tf.train.Saver()
+
+    saver.restore(sess, 'model_path/model.ckpt')
+
+    print('session restored.')
+
+    batch_size = 100
+    num_data = len(data_paths)
+    num_iter = num_data // batch_size + 1
+
+    result_dir = os.path.join('predict_results', datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    os.makedirs(result_dir)
+    with open(os.path.join(result_dir, 'result.csv'), 'w') as f:
+      for i in range(num_iter):
+        end = (i + 1) * batch_size if (i + 1) * batch_size < num_data else None
+        batch_paths = data_paths[i * batch_size: end]
+        batch = np.array([plt.imread(p) for p in batch_paths])
+        res_cls, res_loc = sess.run([y_test_conv, y_test_loc], feed_dict={x: batch, keep_prob: 1.0})
+
+        for name, cls, loc in zip(batch_paths, res_cls, res_loc):
+          f.write('%s, %s\n' % (extract_img_id(name), generate_rle(loc) if has_bp(cls) else ''))
+
+
+data_paths = read_test_data('../test/')
+predict(data_paths)
--- ultra_detection/main.py
+++ ultra_detection/main.py
@@ -1,77 +1,10 @@
-import math
 import os
 from datetime import datetime
 
 import tensorflow as tf
 
+from ultra_detection.model import inference, loss, training
 from ultra_detection.input_data import read_data_sets
-
-
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-
-def inference(keep_prob, x_image):
-  with tf.name_scope('conv1'):
-    # conv 1
-    weights = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=1.0 / math.sqrt(420 * 580 / 2), name='weights'))
-    biases = tf.Variable(tf.constant(0.01, shape=[32], name='biases'))
-
-    # relu + pool
-    h_conv1 = tf.nn.relu(conv2d(x_image, weights) + biases)
-    h_pool1 = max_pool_2x2(h_conv1)
-  with tf.name_scope('conv2'):
-    # conv 2
-    weights = tf.Variable(
-      tf.truncated_normal([5, 5, 32, 64], stddev=1.0 / math.sqrt(210 * 290 * 32 / 2), name='weights'))
-    biases = tf.Variable(tf.constant(0.01, shape=[64], name='biases'))
-
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, weights) + biases)
-    h_pool2 = max_pool_2x2(h_conv2)
-  h_pool2_flat = tf.reshape(h_pool2, [-1, 105 * 145 * 64])
-  with tf.name_scope('fc1'):
-    # fc 1
-    weights = tf.Variable(
-      tf.truncated_normal([105 * 145 * 64, 128], stddev=1.0 / math.sqrt(105 * 145 * 64 / 2), name='weights'))
-    biases = tf.Variable(tf.constant(0.01, shape=[128], name='biases'))
-
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, weights) + biases)
-
-    # dropout
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-  with tf.name_scope('cls_head'):
-    # fc 2
-    weights = tf.Variable(tf.truncated_normal([128, 2], stddev=1.0 / math.sqrt(105 * 145 * 64 / 2), name='weights'))
-    biases = tf.Variable(tf.constant(0.01, shape=[2], name='biases'))
-
-    # softmax
-    y_cls = tf.nn.softmax(tf.matmul(h_fc1_drop, weights) + biases)
-  with tf.name_scope('loc_head'):
-    # fc 2
-    weights = tf.Variable(tf.truncated_normal([128, 4], stddev=1.0 / math.sqrt(105 * 145 * 64 / 2), name='weights'))
-    biases = tf.Variable(tf.constant(0.01, shape=[4], name='biases'))
-
-    y_loc = tf.matmul(h_fc1_drop, weights) + biases
-  return y_cls, y_loc
-
-
-def loss(y_cls, y_loc, y_train_cls, y_train_loc):
-  cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_cls * tf.log(y_train_cls), reduction_indices=[1]), name='xentropy')
-  l2 = tf.reduce_mean(tf.reduce_sum(tf.squared_difference(y_loc, y_train_loc), reduction_indices=[1]), name='l2_loss')
-  return cross_entropy, l2
-
-
-def training(cross_entropy, l2):
-  tf.scalar_summary(cross_entropy.op.name, cross_entropy)
-  tf.scalar_summary(l2.op.name, l2)
-  # solver
-  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy + l2)
-  return train_step
 
 
 def save_model(saver, sess):
