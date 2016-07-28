6b4b43c199d7f57d07c98ecde3149007c82a921b
Tong Damon Da <tongda@outlook.com>
2016-07-28 21:36:20 +0800

    #3 [tongda] fix typo

--- ultra_detection/model.py
+++ ultra_detection/model.py
@@ -17,7 +17,7 @@ def conv(filter_size, num_filter, images, is_training=True):
   biases = tf.Variable(tf.constant(0.01, shape=[num_filter]), name='biases')
   conv = tf.nn.conv2d(images, weights, strides=[1, 1, 1, 1], padding='SAME') + biases
   h_norm = batch_norm(conv, center=True, scale=True, is_training=is_training, reuse=True, scope=conv.op.name)
-  relu = tf.nn.elu(h_norm_1, name='conv_relu')
+  relu = tf.nn.elu(h_norm, name='conv_relu')
   _activation_summary(relu)
   return relu
 
